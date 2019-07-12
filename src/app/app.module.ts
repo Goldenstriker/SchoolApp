3840c3af150a47860c5ebae48ebc9909873f98ff
Goldenstriker <ndparab240@gmail.com>
2019-07-12 17:50:16 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ,AppRoutingModule],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
