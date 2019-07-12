ba644e08c6662cb938866c2f68ebf70cb95367f1
Goldenstriker <ndparab240@gmail.com>
2019-07-12 11:21:40 +0530

    3 files added

---
+++ src/app/student/student.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentlistComponent } from './studentlist/studentlist.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [StudentlistComponent]
+})
+export class StudentModule { }
