79314577a97f08ed80d46c77af9674ff8071f462
Goldenstriker <ndparab240@gmail.com>
2019-07-12 11:46:34 +0530

    1 file modified

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentlistComponent } from './studentlist/studentlist.component';
-
+import {StudentRoutingModule} from './student-routing.module';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    StudentRoutingModule
   ],
   declarations: [StudentlistComponent]
 })
