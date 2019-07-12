a67ac0f0979cc4ff5b329edcafa728b6f959d6f4
Goldenstriker <ndparab240@gmail.com>
2019-07-12 17:56:41 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import { CardComponent } from './card/card.component';
+import {StudentModule} from './student/student.module';
+import {TeacherModule} from './teacher/teacher.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,AppRoutingModule],
+  imports:      [ BrowserModule, FormsModule ,AppRoutingModule,TeacherModule,StudentModule],
   declarations: [ AppComponent, CardComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  exports: [CardComponent]
 })
 export class AppModule { }
--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { StudentlistComponent } from './studentlist/studentlist.component';
 import {StudentRoutingModule} from './student-routing.module';
 import { StudenteditComponent } from './studentedit/studentedit.component';
+import { CardComponent } from '../card/card.component';
 @NgModule({
   imports: [
     CommonModule,
-    StudentRoutingModule
+    StudentRoutingModule,
+    CardComponent
   ],
   declarations: [StudentlistComponent, StudenteditComponent]
 })
