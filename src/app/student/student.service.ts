6d9863b42ae04458ac46d13dac936ebc981709aa
Goldenstriker <ndparab240@gmail.com>
2019-07-15 18:59:41 +0530

    1 file modified, 1 added

--- src/app/student/student.service.ts
+++ src/app/student/student.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
+import {Student} from  './student'; 
 @Injectable()
 export class StudentService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
 baseURL:string = "https://my-json-server.typicode.com/Goldenstriker/schoolappdb/";
+
+  get_students(){
+    return this.httpClient.get<Student[]>(this.baseURL+'/student');
+  }
+  get_student(id:number){
+    return this.httpClient.get<Student>(this.baseURL+'/student/'+id);
+  }
+  delete_student(id:number){
+    return this.httpClient.delete(this.baseURL+'/student/'+id);
+  }
+
 }
---
+++ src/app/student/student.ts
@@ -0,0 +1,6 @@
+export class Student {
+  id:number;
+  email: string;
+  first_name: string;
+  last_name: number;
+}
