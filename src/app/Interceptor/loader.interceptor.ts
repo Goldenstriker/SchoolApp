e3edcac53876943433a3222c715c5ec9f47b8f30
Goldenstriker <ndparab240@gmail.com>
2019-07-24 19:46:09 +0530

    1 file modified

--- src/app/Interceptor/loader.interceptor.ts
+++ src/app/Interceptor/loader.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { finalize } from "rxjs/operators";
+import { finalize,delay } from "rxjs/operators";
 
 import { LoadingserviceService } from '../LoadingService/loadingservice.service';
 
@@ -13,6 +13,7 @@ export class LoaderInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         this.loaderService.show();
         return next.handle(req).pipe(
+            delay(3000),
             finalize(() => { this.loaderService.hide();console.log("it working");})
         );
     }
