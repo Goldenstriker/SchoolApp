bc502f9188e390901bde055d4b5b8fb610545e26
Goldenstriker <ndparab240@gmail.com>
2019-07-25 20:34:05 +0530

    1 file modified

--- src/app/teacher/teacher.module.ts
+++ src/app/teacher/teacher.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { TeacherlistComponent } from './teacherlist/teacherlist.component';
 import {TeacherRoutingModule} from './teacher-routing.module';
 import { TeacherService } from './teacher.service';
-import { CardComponent } from '../card/card.component';
 @NgModule({
   imports: [
     CommonModule,
