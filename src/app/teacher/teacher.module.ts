cbc5dd3d54a9347cc2ed9ee7a8e34cc0524df941
Goldenstriker <ndparab240@gmail.com>
2019-07-25 20:27:40 +0530

    1 file modified

--- src/app/teacher/teacher.module.ts
+++ src/app/teacher/teacher.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { TeacherlistComponent } from './teacherlist/teacherlist.component';
 import {TeacherRoutingModule} from './teacher-routing.module';
 import { TeacherService } from './teacher.service';
+import { CardComponent } from '../card/card.component';
 @NgModule({
   imports: [
     CommonModule,
     TeacherRoutingModule
   ],
-  declarations: [TeacherlistComponent],
+  declarations: [TeacherlistComponent,CardComponent],
   providers: [TeacherService]
 })
 export class TeacherModule { }
